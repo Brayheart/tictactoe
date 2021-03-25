2759a11e5ced56cd906cb5ead6b0c343db25285a
Tyler Bray <broncobray77@gmail.com>
2021-03-25 12:01:54 -0700

    check horziontal/vertical

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ class Player {
     }
 }
 
-
 class Board {
     constructor() {
         this.grid = [
@@ -36,11 +35,36 @@ class Board {
       // if the counter !== 3 then i did not win that row, continue to next row
       // if counter === 3 then i won and can return true and not continue checking other rows
       // if i go through all the rows without returning true, return false
-    
+      var counter = 0
+      this.grid.forEach((row) => {
+        row.forEach(el => {
+            if(el === sym){
+                counter++
+            }
+        })
+      })
+        if(counter === 3){
+            return "Win"
+        }
+        return 'false'
     }
 
-    checkWinVeritcally() {
+    checkWinVeritcally(sym) {
+        for (let x = 0; x < this.grid.length; x++) {
+            var counter = 0
 
+            for (let y = 0; y < this.grid.length; y++) {
+                if(this.grid[y][x] === sym){
+                    counter++
+                }    
+            }
+
+            if(counter === 3){
+                return "Win"
+            }
+            
+        }
+        return 'false'
     }
 
     checkWinDiagonally() {
@@ -48,7 +72,6 @@ class Board {
     }
 
 }
-
 
 class Game {
     constructor(name1, sym1, name2, sym2) {
@@ -58,16 +81,10 @@ class Game {
     }
 }
 
-
-
 var player1 = window.prompt("Player one, what is your name")
 var sym1 = window.prompt(`${player1} what is your symbol?`)
 var player2 = window.prompt("Player one, what is your name")
 var sym2 = window.prompt(`${player2} what is your symbol?`)
-
-
-
-
 
 const game = new Game(player1, sym1, player2, sym2)
 console.log(game.board.printGrid())
